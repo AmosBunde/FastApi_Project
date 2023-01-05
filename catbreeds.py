d63a222646e8311768a8b8a651c3b6c8dcd4093a
AmosBunde <git.bunde@hotmail.com>
2023-01-05 20:54:11 +0300

    api get dev stage commit

--- catbreeds.py
+++ catbreeds.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,9 +14,16 @@ CATS = {
 
 }
 
+
+
 @app.get("/")
-async def get_cat_breeds():
+async def get_cat_breeds(skip_cat: Optional[str] =None):
+    if skip_cat:
+        new_cats = CATS.copy()
+        del new_cats[skip_cat]
+        return new_cats
     return CATS
+   
 
 @app.get("/cats/mycats")
 async def get_my_favourite_cat():
@@ -23,3 +32,17 @@ async def get_my_favourite_cat():
 @app.get("/cats/{breed}")
 async def pick_cats(breed: str):
     return {"breed": breed}    
+
+
+@app.post("/")
+async def create_cat(cat_breed, cat_size):
+    current_cat_id = 0
+
+    if len(CATS)> 0:
+        for cat in CATS:
+            x = int(cat.split('_')[])
+            if x > current_cat_id:
+                current_cat_id = x
+
+    CATS[f'cat_{current_cat_id + 1}'] = {'breed': cat_breed, 'size': cat_size}
+    return CATS[f'cat_{current_cat_id + 1}']
